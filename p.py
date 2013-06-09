53a6f1830ef808e32b30f694da76ff57ae8de0f2
clowwindy <clowwindy42@gmail.com>
2013-06-09 17:45:27 +0800

    make it shorter

--- p.py
+++ p.py
@@ -6,43 +6,24 @@ HEAD = 'fuck fangbinxing\r\n'
 if 'client' in sys.argv:
     listen_port = 8080
     _connectTCP = reactor.connectTCP
-
-    def connectTCP_(host, p, factory):
-        _connectTCP(sys.argv[2], 1984, factory)
-
-    reactor.connectTCP = connectTCP_
-
-    def _sendCommand(self, command, path):
-        # self.transport.writeSequence(HEAD)
-        self.sendCommand_(command, path)
-
-    http.HTTPClient.sendCommand_ = http.HTTPClient.sendCommand
-    http.HTTPClient.sendCommand = _sendCommand
+    reactor.connectTCP = lambda host, p, factory: _connectTCP(sys.argv[2], 1984, factory)  # redirect to our server
+    _sendCommand = http.HTTPClient.sendCommand
+    http.HTTPClient.sendCommand = lambda self, command, path: _sendCommand(self, HEAD + command, path)  # prepend something
 elif 'server' in sys.argv:
     listen_port = 1984
+
     def process_(self):
         headers = self.getAllHeaders().copy()
         parsed = headers['host'].split(':')
-        host = parsed[0]
-        port = 80 if len(parsed) == 1 else int(parsed(1))
         self.content.seek(len(HEAD), 0)
-        clientFactory = proxy.ProxyClientFactory(self.method, self.uri,
-                                                 self.clientproto, headers,
-                                                 self.content.read(), self)
-        self.reactor.connectTCP(host, port, clientFactory)
-
-    def _lineReceived(self, line):
-        print line
-        if line.strip() == HEAD.strip():
-            return
-        else:
-            http.HTTPChannel.lineReceived_(self, line)
+        clientFactory = proxy.ProxyClientFactory(self.method, self.uri, self.clientproto, headers, self.content.read(), self)
+        self.reactor.connectTCP(parsed[0], 80 if len(parsed) == 1 else int(parsed(1)), clientFactory)
 
     proxy.ProxyRequest.process = process_
-    http.HTTPChannel.lineReceived_ = http.HTTPChannel.lineReceived
-    http.HTTPChannel.lineReceived = _lineReceived
+    _lineReceived = http.HTTPChannel.lineReceived
+    http.HTTPChannel.lineReceived = lambda self, line: _lineReceived(self, line) if line.strip() != HEAD.strip() else None
 else:
-    print 'p.py client/server [server_ip]'
+    print 'p.py server\np.py client server_ip'
     sys.exit(1)
 
 f = http.HTTPFactory()
